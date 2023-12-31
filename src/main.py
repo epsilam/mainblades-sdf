41f2fb8c61a853133ac5ad97c0a0b074160d5660
Laurens Espada <2.7182@protonmail.com>
2023-12-31 12:10:40 +0100

    Use PIL to replicate polygon images

--- src/main.py
+++ src/main.py
@@ -1,30 +1,39 @@
 from tkinter import *
 from tkinter import ttk
 import numpy as np
+from PIL import Image, ImageDraw
 
 class Sketchpad(Canvas):
     def __init__(self, parent, **kwargs):
         super().__init__(parent, **kwargs)
         self.bind("<Button-1>", self.add_point)
-        #self.bind("<B1-Motion>", self.add_line)
-        self.points = np.empty(shape=(0,2), dtype=int)
-        self.polygon = None
         self.create_text((110,20), text="Click anywhere to place a vertex.")
+        self.vertices = []
+        self.polygon = None
         
     def add_point(self, event):
-        self.points = np.vstack([self.points, [event.x, event.y]])
-        print(self.points)
+        self.vertices += [event.x, event.y]
         self.update_polygon()
             
     def update_polygon(self):
-        if self.points.shape[0] != 0:
+        if len(self.vertices)>=4: # if at least 2 vertices have been placed
             self.delete(self.polygon)
             self.polygon = self.create_polygon(
-                np.reshape(self.points, (1,-1)).tolist(), 
+                self.vertices,
                 fill="white",
-                outline="white")
-    #def update_sdf(self):
-        
+                outline="white"
+            )
+
+            canvas_width = self.winfo_width()
+            canvas_height = self.winfo_height()
+            self.image = Image.new("RGB",(canvas_width, canvas_height),"black")
+            self.draw = ImageDraw.Draw(self.image)
+            print(self.vertices)
+            self.draw.polygon(
+                self.vertices,
+                fill="white",
+                outline="white"
+            )
         
 root = Tk()
 root.columnconfigure(0, weight=1)
